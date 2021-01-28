6b0aa402d600696766a3c6b2d0505df15edd4325
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-7TUSQ40>
2021-01-28 12:10:55 -0300

    Projetin véi de guerra!

---
+++ index.php
@@ -0,0 +1,89 @@
+<?php
+
+include_once './painel/bd/conexao.php';
+include_once 'painel/helper/funcoes.php';
+
+$pg = isset($_GET['pg']);
+
+if ($pg) {
+    //existe
+    switch ($_GET['pg']) {
+
+        case 'inicio':
+            include_once 'painel/paginas/includes/header.php';
+            include_once 'painel/paginas/includes/menus.php';
+            include_once 'painel/paginas/dashboard.php';
+            include_once 'painel/paginas/includes/footer.php';
+            break;
+
+        case 'produtos':
+            //Fazer uma consulta no banco e disponibilizar os resultados
+            //$pessoa = new Pessoa();
+
+            $resultDados = new Conexao();
+            $dados = $resultDados->selecionaDados('SELECT * FROM produtos');
+
+
+            include_once 'painel/paginas/includes/header.php';
+            include_once 'painel/paginas/includes/menus.php';
+            include_once 'painel/paginas/produtos.php';
+            include_once 'painel/paginas/includes/footer.php';
+            break;
+
+        case 'produtos-item':
+            
+            $id = $_GET['id'];
+            
+            $resultDados = new Conexao();
+            $dados = $resultDados->selecionaDados('SELECT * FROM produtos WHERE id = ' . $id);
+            
+            include_once 'painel/paginas/includes/header.php';
+            include_once 'painel/paginas/includes/menus.php';
+            include_once 'painel/paginas/produtos-item.php';
+            include_once 'painel/paginas/includes/footer.php';
+            break;
+
+
+
+
+        case 'servicos':
+            include_once 'painel/paginas/includes/header.php';
+            include_once 'painel/paginas/includes/menus.php';
+            include_once 'painel/paginas/servicos.php';
+            include_once 'painel/paginas/includes/footer.php';
+            break;
+
+        case 'contato':
+            include_once 'painel/paginas/includes/header.php';
+            include_once 'painel/paginas/includes/menus.php';
+            include_once 'painel/paginas/contato.php';
+            include_once 'painel/paginas/includes/footer.php';
+            break;
+
+        case 'login':
+            include_once 'painel/paginas/acesso/login.php';
+            break;
+
+        case 'dashboard':
+            //Página inicial do Painel Adm           
+            if (verificaLogin()) {
+
+                include_once 'painel/paginas/includes/header.php';
+                include_once 'painel/paginas/includes/menus.php';
+                include_once 'painel/paginas/dashboard.php';
+                include_once 'painel/paginas/includes/footer.php';
+            } else {
+                echo 'Login ou senha inválidos.';
+            }
+            break;
+
+        default:
+            include_once 'painel/paginas/dashboard.php';
+            break;
+    }
+} else {
+    //não existe
+    include_once 'painel/paginas/dashboard.php';
+}
+
+
